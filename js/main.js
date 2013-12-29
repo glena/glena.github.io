4d0766fc6bb7a77d55d4d0463eae71aa00c40d31
German <german.lena@gmail.com>
2013-12-29 14:31:49 -0300

    images and background

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+function pad(b,d){for(var c=b+"";c.length<d;)c="0"+c;return c}
+function shuffle(b){for(var d=b.length,c,e;d--;)e=Math.random()*d|0,c=b[d],b[d]=b[e],b[e]=c;return b}
+for(var images=[],a=1;17>=a;a++)images.push("../img/image"+pad(a,3)+".jpg");
+images=shuffle(images);
+$.backstretch(images,{duration:4000,fade:750});
