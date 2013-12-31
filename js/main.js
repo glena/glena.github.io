e7188b19cb78675df2fdd28ea2382c1cad40db0b
German <german.lena@gmail.com>
2013-12-31 20:51:18 -0300

    integracion copada

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$.backstretch("../img/backgrounds/image015.jpg");
+$(document).ready(function(){
+	checkHash();
+
+	$('a').click(function(){
+		_gaq.push(['_trackEvent', 'link', 'click', this.href]);
+	    //ga('send', 'event', 'link', 'click', this.href);
+	});
+});
+function checkHash()
+{
+	var hash = window.location.hash;
+	_gaq.push(['_trackEvent', 'link', 'click', hash]);
+	//ga('send', 'event', 'link', 'click', hash);
+    loadArticle(hash);
+}
+function showArticle(url)
+{
+    var hash = '#' + url.split('#').pop();
+    loadArticle(hash);
+}
+function loadArticle(hash)
+{
+    var pages = {
+        '#about-me':'.main',
+        '#experience':'.resume',
+    };
+    var selector = pages[hash];
+    if (selector != undefined)
+    {
+        $('article').hide();
+        $(selector).show();
+    }
+}
